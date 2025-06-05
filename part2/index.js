2a9216ccd60a6a90642d32cedd9e6e23c8317f30
Damian Cazal <damiancazal21@gmail.com>
2025-06-05 14:21:19 -0300

    Se agrego validaciones

--- part2/index.js
+++ part2/index.js
@@ -36,7 +36,7 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -50,19 +50,21 @@ app.post('/api/notes', (request, response) => {
     important: Boolean(body.important) || false,
   })
 
-  note.save().then(savedNote => response.json(savedNote))
+  note.save()
+    .then(savedNote => response.json(savedNote))
+    .catch(error => next(error))
 
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
+  const { content, important } = request.body
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -74,7 +76,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
